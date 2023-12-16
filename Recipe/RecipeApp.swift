1ba9a2f6050283879e4b9b4a1f3c7139509b3323
TaeVon Lewis <65370736+taevonlewis@users.noreply.github.com>
2023-12-15 19:14:37 -0500

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Copyright © 2023 TaeVon Lewis. All rights reserved.
+//
+
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
